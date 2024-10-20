21bede18cb94812d6d2fa08362704582c150d1ac
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2024-10-20 16:52:14 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let hostSprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let number = randint(1, 10)
+let guess = 0
+while (false) {
+	
+}
+if (guess == number) {
+    hostSprite.sayText("Great job!")
+} else {
+    hostSprite.sayText("Sorry, That's not my number.")
+}
